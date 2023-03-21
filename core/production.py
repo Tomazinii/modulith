120369c3ca29defb42de8dff804a049bb9155521
alotropo <jhontomazini@hotmail.com>
2023-03-21 15:35:34 -0300

    asdf

--- core/production.py
+++ core/production.py
@@ -1,6 +1,12 @@
 from .development import *
 
 import os
+os.environ["PYTHON_ENV"] = "production"
+os.environ["DATABASE_URL"] = "postgresql://postgres:azsxdcfvgb@awseb-e-nmpk22gzwr-stack-awsebrdsdatabase-2gwybje6iodk.cbo3ynifpjlw.sa-east-1.rds.amazonaws.com:5432/postgres"
+os.environ["HOST"] = "awseb-e-nmpk22gzwr-stack-awsebrdsdatabase-2gwybje6iodk.cbo3ynifpjlw.sa-east-1.rds.amazonaws.com"
+os.environ["DATABASE"] = "postgres"
+os.environ["USER"] = "postgres"
+os.environ["PASSWORD"] = "azsxdcfvgb"
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 HOST = os.environ.get("HOST") 
